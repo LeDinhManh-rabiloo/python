7d6e485d32d07d50ada54bcd904a254704946332
Manhga <manhga3689@gmail.com>
2018-08-01 19:52:00 +0700

    Tow commit

--- Basic4/connectDB.py
+++ Basic4/connectDB.py
@@ -1,2 +1,3 @@
 import psycopg2
-conn = psycopg2.connect(database='Itplusdb')
+conn = psycopg2.connect(database='Itplusdb',user = 'postgres',password='manhg@290198',host='127.0.0.1',port='5432')
+print(conn)
